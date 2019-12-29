cdbf7a317a521e6bdf3f022a22b2849d620b2bd1
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-29 21:39:52 +0900

    spring initializer

---
+++ src/main/kotlin/writory/WritoryApplication.kt
@@ -0,0 +1,11 @@
+package writory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WritoryApplication
+
+fun main(args: Array<String>) {
+    runApplication<WritoryApplication>(*args)
+}
