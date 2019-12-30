943da074f5acca0a8323d09e3578a82801bb3282
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-30 19:07:39 +0900

    item entity, repository, item section entity, repository を追加

---
+++ src/main/kotlin/writory/entity/ItemEntity.kt
@@ -0,0 +1,38 @@
+package writory.entity
+
+import java.time.LocalDateTime
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "item")
+class ItemEntity(
+        var userId: String? = null,
+        var title: String? = null
+) {
+
+    @Id
+    var id: String? = null
+        private set
+
+    var created: LocalDateTime? = null
+        private set
+
+    var modified: LocalDateTime? = null
+        private set
+
+    @PrePersist
+    fun prePersist() {
+        id = UUID.randomUUID().toString()
+
+        val now: LocalDateTime = LocalDateTime.now()
+        created = now
+        modified = now
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        modified = LocalDateTime.now()
+    }
+
+}
--- src/main/resources/db/migration/V1__initial_version.sql
+++ src/main/resources/db/migration/V1__initial_version.sql
@@ -9,3 +9,31 @@ create table user
     unique key (email)
 ) engine = InnoDB
   default charset utf8;
+
+create table item
+(
+    id       varchar(36) not null,
+    user_id  varchar(36) not null,
+    title    text        not null,
+    created  timestamp   not null default 0,
+    modified timestamp   not null default 0,
+    primary key (id),
+    foreign key (user_id) references user (id) on delete cascade on update cascade
+) engine = InnoDB
+  default charset utf8;
+
+create table item_section
+(
+    id       varchar(36) not null,
+    item_id  varchar(36) not null,
+    position int         not null,
+    header   text        not null,
+    body     text        not null,
+    star     tinyint(1)  not null,
+    created  timestamp   not null default 0,
+    modified timestamp   not null default 0,
+    primary key (id),
+    foreign key (item_id) references item (id) on delete cascade on update cascade,
+    unique key (item_id, position)
+) engine = InnoDB
+  default charset utf8;
