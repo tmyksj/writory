a00e0ea9a91653dcbfb64dee6f1a58aba368898a
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-08 14:43:01 +0900

    バリデーションの修正

--- src/main/kotlin/writory/application/item/form/ItemForm.kt
+++ src/main/kotlin/writory/application/item/form/ItemForm.kt
@@ -1,12 +1,15 @@
 package writory.application.item.form
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 data class ItemForm(
 
         @field:NotBlank
+        @field:Size(max = 255, min = 0)
         var id: String? = null,
 
+        @field:Size(max = 8, min = 0)
         var all: String? = null
 
 )
