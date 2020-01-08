f008c824741f55fdd3a8c7aece6cbecd7292b714
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-08 14:49:51 +0900

    バリデーションの修正

--- src/main/kotlin/writory/application/dashboard/form/ItemDeleteForm.kt
+++ src/main/kotlin/writory/application/dashboard/form/ItemDeleteForm.kt
@@ -1,10 +1,12 @@
 package writory.application.dashboard.form
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 data class ItemDeleteForm(
 
-        @NotBlank
+        @field:NotBlank
+        @field:Size(max = 255, min = 0)
         var id: String? = null
 
 )
