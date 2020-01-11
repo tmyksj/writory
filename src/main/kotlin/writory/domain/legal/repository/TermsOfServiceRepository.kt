629968ecb616796046de2632e4d22bec06cccfd6
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-11 11:13:39 +0900

    型を修正

--- src/main/kotlin/writory/domain/legal/repository/TermsOfServiceRepository.kt
+++ src/main/kotlin/writory/domain/legal/repository/TermsOfServiceRepository.kt
@@ -2,12 +2,11 @@ package writory.domain.legal.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import writory.domain.legal.entity.PrivacyPolicyEntity
 import writory.domain.legal.entity.TermsOfServiceEntity
 
 @Repository
 interface TermsOfServiceRepository : JpaRepository<TermsOfServiceEntity, String> {
 
-    fun findFirstByOrderByModifiedDesc(): PrivacyPolicyEntity?
+    fun findFirstByOrderByModifiedDesc(): TermsOfServiceEntity?
 
 }
